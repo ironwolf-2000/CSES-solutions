03050490a44302fb79cb4b82b407161545c4dbf4
mevologodskiy <ironwolf-2000@users.noreply.github.com>
2024-06-15 18:09:21 +0300

    add solution to Dynamic_Range_Sum_Queries

---
+++ Range Queries/Dynamic_Range_Sum_Queries/solution.py
@@ -0,0 +1,59 @@
+from math import ceil, log2
+import sys
+
+
+def read_numbers():
+    return map(int, sys.stdin.readline().split())
+
+
+class SegmentTree:
+    def __init__(self, A: "list[int]") -> None:
+        self.n = 2 ** ceil(log2(len(A)))
+        self.tree = [0] * (self.n * 2)
+
+        for k, x in enumerate(A):
+            self.set(k, x)
+
+    def set(self, k: int, x: int) -> None:
+        k += self.n
+
+        self.tree[k] = x
+        k //= 2
+
+        while k >= 1:
+            self.tree[k] = self.tree[k * 2] + self.tree[k * 2 + 1]
+            k //= 2
+
+    def sum(self, a: int, b: int) -> int:
+        a += self.n
+        b += self.n
+
+        s = 0
+
+        while a <= b:
+            if a % 2 == 1:
+                s += self.tree[a]
+                a += 1
+
+            if b % 2 == 0:
+                s += self.tree[b]
+                b -= 1
+
+            a //= 2
+            b //= 2
+
+        return s
+
+
+n, q = read_numbers()
+A = list(read_numbers())
+
+tree = SegmentTree(A)
+
+for _ in range(q):
+    t, x, y = read_numbers()
+
+    if t == 1:
+        tree.set(x - 1, y)
+    else:
+        print(tree.sum(x - 1, y - 1))
