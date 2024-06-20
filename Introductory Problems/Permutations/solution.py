75591ba3fba3eec3f33a896cec8f8194321fb48f
mevologodskiy <ironwolf-2000@users.noreply.github.com>
2024-06-21 00:16:39 +0300

    add solution to Permutations

---
+++ Introductory Problems/Permutations/solution.py
@@ -0,0 +1,13 @@
+import sys
+
+
+n = int(sys.stdin.readline())
+
+if n == 2 or n == 3:
+    print("NO SOLUTION")
+else:
+    for i in range(2, n + 1, 2):
+        print(i, end=" ")
+
+    for i in range(1, n + 1, 2):
+        print(i, end=" ")
